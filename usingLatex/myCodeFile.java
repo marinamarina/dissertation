736719c402f48de41267757990946604876871ed
Marina Shchukina <marinamiddleton@localhost.local>
2015-04-30 16:22:49 +0100

    a lot of small changes

--- usingLatex/myCodeFile.java
+++
@@ -1,9 +0,0 @@
-public void someInterestingMethod( int x ) {	
-	try {            
-		DataInputStream in = new DataInputStream(req.getInputStream());            
-		HttpSession session = req.getSession(true);
-		//Read Input Data
-		in.close();		
-	} catch ( Exception err ) { }
-	outputStream.close();	
-}
